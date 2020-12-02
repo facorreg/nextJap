add20ed86034fb806efe4b536f4808d4e51309fb
root <root@DESKTOP-51MGIR3.localdomain>
2020-12-02 18:46:21 +0100

    boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': ['off'],
+    'react/no-unescaped-entities': ['off'],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@utils', './src/utils']
+          ['@apollo', './src/apollo']
+          ['@components', './src/components']
+          ['@statuc', './src/static']
+        ]
+      }
+    }
+  }
+};
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import getEnv from './getEnv';
+import { getCookie, setCookie, removeCookie } from './cookies';
+import isServerSide from './isServerSide';
+
+export {
+  getEnv,
+  getCookie,
+  setCookie,
+  removeCookie,
+  isServerSide,
+};
